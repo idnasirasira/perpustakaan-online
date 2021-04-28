cb377d6af6d81dcb565deadf2bae5117f21f65cd
mohammadisa2 <quzeyuah@gmail.com>
2021-04-28 15:07:27 +0700

    partial layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -40,77 +40,7 @@
     
       @include('__partials.navbar');
     
-      <!-- Main Sidebar Container -->
-      <aside class="main-sidebar sidebar-dark-primary elevation-4">
-        <!-- Brand Logo -->
-        <a href="index3.html" class="brand-link">
-          <img src="{{asset('assets/img')}}/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-          <span class="brand-text font-weight-light">Perpustakaan Online</span>
-        </a>
-    
-        <!-- Sidebar -->
-        <div class="sidebar">
-          <!-- Sidebar user panel (optional) -->
-          <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-            <div class="image">
-              <img src="{{asset('assets/img')}}/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
-            </div>
-            <div class="info">
-              <a href="#" class="d-block">{{ auth()->user()->name }}</a>
-            </div>
-          </div>
-    
-          <!-- SidebarSearch Form -->
-          <div class="form-inline">
-            <div class="input-group" data-widget="sidebar-search">
-              <input class="form-control form-control-sidebar" type="search" placeholder="Search" aria-label="Search">
-              <div class="input-group-append">
-                <button class="btn btn-sidebar">
-                  <i class="fas fa-search fa-fw"></i>
-                </button>
-              </div>
-            </div>
-          </div>
-    
-          <!-- Sidebar Menu -->
-          <nav class="mt-2">
-            <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-              <!-- Add icons to the links using the .nav-icon class
-                   with font-awesome or any other icon font library -->
-                   @if (auth()->user()->level == "1")
-              <li class="nav-item">
-                <a href="{{route('book.index')}}" class="nav-link">
-                  <i class="nav-icon fas fa-tachometer-alt"></i>
-                  <p>
-                    Buku
-                  </p>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a href="{{route('sewa.index')}}" class="nav-link">
-                  <i class="nav-icon fas fa-tachometer-alt"></i>
-                  <p>
-                    Sewa
-                  </p>
-                </a>
-              </li>
-              @endif
-                  <li class="nav-item">
-                  @if (auth()->user()->level == "2")
-                    <a href="{{route('user.index')}}" class="nav-link">
-                      <i class="far fa-circle nav-icon"></i>
-                      <p>Pengguna</p>
-                    </a>
-                  </li>
-                  @endif
-                </ul>
-              </li>
-            </ul>
-          </nav>
-          <!-- /.sidebar-menu -->
-        </div>
-        <!-- /.sidebar -->
-      </aside>
+      @include('__partials.main_sidebar')
     
       <!-- Content Wrapper. Contains page content -->
       <div class="content-wrapper">
@@ -118,29 +48,9 @@
       </div>
       <!-- /.content-wrapper -->
     
-      <!-- Control Sidebar -->
-      <aside class="control-sidebar control-sidebar-dark">
-        <!-- Control sidebar content goes here -->
-        <div class="p-3">
-          <h5>Profile</h5>
-          <p></p>
-          <form id="logout-form" action="{{route('logout')}}" method="post">
-            @csrf
-          </form>
-          <a href="#" onclick="$('#logout-form').submit()">Logout</a>
-        </div>
-      </aside>
-      <!-- /.control-sidebar -->
+      @include('__partials.control_sidebar')
     
-      <!-- Main Footer -->
-      <footer class="main-footer">
-        <!-- To the right -->
-        <div class="float-right d-none d-sm-inline">
-          Anything you want
-        </div>
-        <!-- Default to the left -->
-        <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.
-      </footer>
+      @include('__partials.main_footer')
     </div>
 </body>
 
